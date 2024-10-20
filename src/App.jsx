565dbb7d1b598a005a63053d675557aa4ed930d4
Sanket Ghoagre <sanket_vasant.ghogare@syngenta.com>
2024-10-20 17:48:42 +0530

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Navbar from './Navbar/Navbar'
 import Comment from './Comment/Comment'
 import Page1 from './Page1/Page1'
 import { HelmetProvider } from 'react-helmet-async';
+import ReactGA from 'react-ga4';
 
+const Tracking_ID = "G-Y40N5NXK81";
+ReactGA.initialize(Tracking_ID);
 
 function App() {
 
